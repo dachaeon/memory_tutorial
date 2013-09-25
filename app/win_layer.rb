812dad47e034de6b19f945436a75800932c341f8
Crockett <crockett.hopper@nestwise.com>
2013-09-24 21:31:39 -0700

    added ability to play again

--- app/win_layer.rb
+++ app/win_layer.rb
@@ -4,5 +4,14 @@ class WinLayer < Joybox::Core::Layer
   def on_enter
     title = Label.new text: "You Win!", font_size: 48, position: [Screen.half_width, Screen.half_height + 50]
     self << title
+    handle_touches
+  end
+
+  def handle_touches
+    puts "step1"
+    on_touches_ended do |touch, event|
+      puts "step2"
+      Joybox.director.replace_scene GridLayer.scene
+    end
   end
 end

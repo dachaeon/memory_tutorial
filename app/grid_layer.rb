0b7d17855c7e05f668dc6d8bec5f4fa7250172c6
Crockett <crockett.hopper@nestwise.com>
2013-09-24 17:46:04 -0700

    now you can actually win the game

--- app/grid_layer.rb
+++ app/grid_layer.rb
@@ -18,25 +18,55 @@ class GridLayer < Joybox::Core::Layer
   end
 
   def load_tiles
+    types = 8.times.map{ Tile::TYPES.sample} * 2
+
     @tiles = 4.times.map do |row|
       4.times.map do |column|
-        Tile.new position: [column * TILE_SIZE + LEFT_GUTTER, row * TILE_SIZE + BOTTOM_GUTTER]
+        Tile.new type: types.shift, position: [column * TILE_SIZE + LEFT_GUTTER, row * TILE_SIZE + BOTTOM_GUTTER]
       end
     end.flatten
     @tiles.each{|t| @sprite_batch << t}
   end
 
   def handle_touches
+    @active_tiles = []
+
     on_touches_ended do |touches, event|
+      break if @active_tiles.size == 2
+
       touch = touches.any_object
       if tile = tile_to_flip(touch.location)
-        puts 'flipping!'
+        tile.flip
+        @active_tiles << tile
+
+        if @active_tiles.size == 2
+          self.runAction Sequence.with(actions: [Delay.time(by: 1), Callback.with(&handle_active_tiles)])
+        end
+
       end
     end
   end
 
   def tile_to_flip(touch_location)
     @tiles.detect{|t| t.touched?(touch_location)}
+  end
+
+  def handle_active_tiles
+    Proc.new do
+      if @active_tiles.map{|t| t.type }.uniq.size == 1
+        @active_tiles.each(&:freeze)
+        if all_matched?
+          Joybox.director.replace_scene WinLayer.scene
+        end
+      else
+        @active_tiles.each(&:flip)
+      end
+      @active_tiles = []
+    end
+  end
+
+  def all_matched?
+    @tiles.all?(&:frozen)
   end
 
 end
